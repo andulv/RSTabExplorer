808a81b5e29ae51394fb9c7dc2e6539f38c9176d
James EdJo <jamedjo@gmail.com>
2014-05-07 08:41:09 +0100

    Added slide toggle for compact/no-score mode.

---
+++ RockSmithTabExplorer/Controls/SlideToggle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RockSmithTabExplorer
+{
+    public class SlideToggle : CheckBox
+    {
+        static SlideToggle()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SlideToggle), new FrameworkPropertyMetadata(typeof(SlideToggle)));
+        }
+
+        public object On
+        {
+            get { return GetValue(OnProperty); }
+            set { SetValue(OnProperty, value); }
+        }
+
+        public static readonly DependencyProperty OnProperty =
+            DependencyProperty.Register("On", typeof(object), typeof(SlideToggle), null);
+
+        public object Off
+        {
+            get { return GetValue(OffProperty); }
+            set { SetValue(OffProperty, value); }
+        }
+
+        public static readonly DependencyProperty OffProperty =
+            DependencyProperty.Register("Off", typeof(object), typeof(SlideToggle), null);
+    }
+}
